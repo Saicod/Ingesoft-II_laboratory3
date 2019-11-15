3a04f2f35d5a62b7d9d36571429a2880566e211a
Saicod <acastillos@unal.edu.co>
2019-11-15 11:29:56 -0500

    Builder Pattern Implementation

---
+++ punto-3/HawaianPizza.java
@@ -0,0 +1,12 @@
+public class HawaianPizza extends Pizza {
+
+    @Override
+    public float price() {
+       return 12.5f;
+    }
+ 
+    @Override
+    public String name() {
+       return "Hawaian Pizza";
+    }
+ }
